c07712ab4a5759d6095d5000cfe3e81a1acfc3f5
Krylov Maxim <maksimkrylov867@gmail.com>
2026-03-30 15:30:08 +0300

    Добавьте файлы проекта.

---
+++ 13/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListApp
+{
+    public class IntegerList
+    {
+        private List<int> numbers;
+
+        public IntegerList(List<int> numbers)
+        {
+            if (numbers == null || numbers.Count == 0)
+                throw new ArgumentException("Список пуст");
+
+            this.numbers = numbers;
+        }
+
+        public double GetAverage()
+        {
+            return numbers.Average();
+        }
+
+        public void RemoveByDeviation(double threshold)
+        {
+            double avg = GetAverage();
+
+            numbers = numbers
+                .Where(x => Math.Abs(x - avg) <= threshold)
+                .ToList();
+        }
+
+        public void SortDescending()
+        {
+            numbers = numbers
+                .OrderByDescending(x => x)
+                .ToList();
+        }
+
+        public List<int> GetList()
+        {
+            return numbers;
+        }
+    }
+}
---
+++ I3/Test1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ListApp;
+using System.Collections.Generic;
+
+namespace ListTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Average()
+        {
+            var list = new IntegerList(new List<int> { 1, 2, 3, 4 });
+            Assert.AreEqual(2.5, list.GetAverage());
+        }
+
+        [TestMethod]
+        public void Test_Sort()
+        {
+            var list = new IntegerList(new List<int> { 1, 5, 3 });
+            list.SortDescending();
+
+            CollectionAssert.AreEqual(
+                new List<int> { 5, 3, 1 },
+                list.GetList()
+            );
+        }
+
+        [TestMethod]
+        public void Test_Remove()
+        {
+            var list = new IntegerList(new List<int> { 1, 2, 100 });
+            list.RemoveByDeviation(10);
+
+            CollectionAssert.AreEqual(
+                new List<int> { 1, 2 },
+                list.GetList()
+            );
+        }
+
+        [TestMethod]
+        public void Test_NoRemove()
+        {
+            var list = new IntegerList(new List<int> { 1, 2, 3 });
+            list.RemoveByDeviation(10);
+
+            CollectionAssert.AreEqual(
+                new List<int> { 1, 2, 3 },
+                list.GetList()
+            );
+        }
+
+        [TestMethod]
+        public void Test_OneElement()
+        {
+            var list = new IntegerList(new List<int> { 5 });
+            Assert.AreEqual(5, list.GetAverage());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void Test_Empty()
+        {
+            new IntegerList(new List<int>());
+        }
+    }
+}
